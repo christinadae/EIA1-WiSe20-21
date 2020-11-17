d70f7215c79c3ffb44c63df27ca792c27acfe084
christinadae <christina.daeschner@hs-furtwangen.de>
2020-11-17 16:00:29 +0100

    start task5

---
+++ Aufgabe5/aufgabe5.js
@@ -0,0 +1,15 @@
+var europa2008 = 4965.7;
+var europa2018 = 4209.3;
+var nordamerika2008 = 6600.4;
+var nordamerika2018 = 6035.6;
+var südamerika2008 = 1132.6;
+var südamerika2018 = 1261.5;
+var afrika2008 = 1028;
+var afrika2018 = 1235.5;
+var asien2008 = 12954.7;
+var asien2018 = 16274.1;
+var australien2008 = 1993;
+var australien2018 = 2100.5;
+var summe2018 = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+console.log("Die Emission von Europa ist:" + europa2018 + "kg CO2");
+//# sourceMappingURL=aufgabe5.js.map
---
+++ Aufgabe5/aufgabe5.ts
@@ -0,0 +1,21 @@
+var europa2008: number = 4965.7;
+var europa2018: number = 4209.3;
+
+var nordamerika2008: number = 6600.4;
+var nordamerika2018: number = 6035.6;  
+
+var südamerika2008: number = 1132.6;
+var südamerika2018: number = 1261.5; 
+
+var afrika2008: number = 1028;
+var afrika2018: number = 1235.5;
+
+var asien2008: number = 12954.7;
+var asien2018: number = 16274.1;
+
+var australien2008: number = 1993;
+var australien2018: number = 2100.5;
+
+var summe2018: number = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+
+console.log("Die Emission von Europa ist:" + europa2018 + "kg CO2")
