067d3dec54ccfde4c4cb7a8fbcd49bdc993109a5
christinadae <christina.daeschner@hs-furtwangen.de>
2020-11-24 21:41:35 +0100

    edit

--- Aufgabe6/aufgabe6.ts
+++ Aufgabe6/aufgabe6.ts
@@ -1,40 +1,43 @@
+
+var GesamtEmission2018: number = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+
 var europa2008: number = 4965.7
 var europa2018: number = 4209.3
-var europaVerursacht: number = Math.round(europa2018/GesamtEmission2018*100)
+var europaVerursacht: number = Math.round((europa2018/GesamtEmission2018)*100)
 var europaVergleichPr: number = Math.round (((europa2018/europa2008)-1)*100)
 var europaVergleichW: number = Math.round (europa2018-europa2008)
 
 
 var nordamerika2008: number = 6600.4;
 var nordamerika2018: number = 6035.6;
-var nordamerikaVerursacht: number = Math.round(nordamerika2018/GesamtEmission2018*100)
+var nordamerikaVerursacht: number = Math.round((nordamerika2018/GesamtEmission2018)*100)
 var nordamerikaVergleichPr: number = Math.round(((nordamerika2018/nordamerika2008)-1)*100)
 var nordamerikaVergleichW: number = Math.round(nordamerika2018-nordamerika2008)
 
 
 var südamerika2008: number = 1132.6;
 var südamerika2018: number = 1261.5;
-var südamerikaVerursacht: number = Math.round(südamerika2018/GesamtEmission2018*100)
+var südamerikaVerursacht: number = Math.round((südamerika2018/GesamtEmission2018)*100)
 var südamerikaVergleichPr: number = Math.round(((südamerika2018/südamerika2008)-1)*100)
 var südamerikaVergleichW: number = Math.round(südamerika2018-südamerika2008)
 
 
 var afrika2008: number = 1028;
 var afrika2018: number = 1235.5;
-var afrikaVerursacht: number = Math.round(afrika2018/GesamtEmission2018*100)
+var afrikaVerursacht: number = Math.round((afrika2018/GesamtEmission2018)*100)
 var afrikaVergleichPr: number = Math.round(((afrika2018/afrika2008)-1)*100)
 var afrikaVergleichW: number = Math.round(afrika2018-afrika2008)
 
 
 var asien2008: number = 12954.7;
 var asien2018: number = 16274.1;
-var asienVerursacht: number = Math.round(asien2018/GesamtEmission2018*100)
+var asienVerursacht: number = Math.round((asien2018/GesamtEmission2018)*100)
 var asienVergleichPr: number = Math.round(((asien2018/asien2008)-1)*100)
 var asienVergleichW: number = Math.round(asien2018-asien2008)
 
 var australien2008: number = 1993;
 var australien2018: number = 2100.5;
-var australienVerursacht: number = Math.round(australien2018/GesamtEmission2018*100)
+var australienVerursacht: number = Math.round((australien2018/GesamtEmission2018)*100)
 var australienVergleichPr: number = Math.round(((australien2018/australien2008)-1)*100)
 var australienVergleichW: number = Math.round(australien2018-australien2008)
 
@@ -45,7 +48,7 @@ function functionEuropa() {
     document.querySelector("#titleRegion").innerHTML ="Europe";
     document.querySelector("#h2-1").innerHTML = europa2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of Europe in 2018";
-    document.querySelector("#h2-2").innerHTML = europaVerursacht +"%";
+    document.querySelector("#h2-2").innerHTML = europaVerursacht + "%";
     document.querySelector("#h2-3").innerHTML = europaVergleichPr +"%";
     document.querySelector("#h2-4").innerHTML = europaVergleichW + "kg CO2";
     }
