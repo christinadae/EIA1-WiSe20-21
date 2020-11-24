ee4542fead54d66f7a4e4a882d9049be9becf6e3
christinadae <christina.daeschner@hs-furtwangen.de>
2020-11-24 21:48:34 +0100

    edit class

--- Aufgabe6/aufgabe6.ts
+++ Aufgabe6/aufgabe6.ts
@@ -46,64 +46,64 @@ var GesamtEmission2018: number = europa2018 + nordamerika2018 + südamerika2018 
 //Europa
 function functionEuropa() {
     document.querySelector("#titleRegion").innerHTML ="Europe";
-    document.querySelector("#h2-1").innerHTML = europa2018 + "";
+    document.querySelector(".h2-1").innerHTML = europa2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of Europe in 2018";
-    document.querySelector("#h2-2").innerHTML = europaVerursacht + "%";
-    document.querySelector("#h2-3").innerHTML = europaVergleichPr +"%";
-    document.querySelector("#h2-4").innerHTML = europaVergleichW + "kg CO2";
+    document.querySelector(".h2-2").innerHTML = europaVerursacht + "%";
+    document.querySelector(".h2-3").innerHTML = europaVergleichPr +"%";
+    document.querySelector(".h2-4").innerHTML = europaVergleichW + "kg CO2";
     }
     document.querySelector(".europe").addEventListener('click', functionEuropa);
 //Nordamerika
     function functionNordamerika() {
     document.querySelector("#titleRegion").innerHTML ="North America";
-    document.querySelector("#h2-1").innerHTML = nordamerika2018 + "";
+    document.querySelector(".h2-1").innerHTML = nordamerika2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of North America in 2018";
-    document.querySelector("#h2-2").innerHTML = nordamerikaVerursacht +"%";
-    document.querySelector("#h2-3").innerHTML = nordamerikaVergleichPr +"%";
-    document.querySelector("#h2-4").innerHTML = nordamerikaVergleichW + "kg CO2";
+    document.querySelector(".h2-2").innerHTML = nordamerikaVerursacht +"%";
+    document.querySelector(".h2-3").innerHTML = nordamerikaVergleichPr +"%";
+    document.querySelector(".h2-4").innerHTML = nordamerikaVergleichW + "kg CO2";
 
 }
 document.querySelector(".northamerica").addEventListener('click', functionNordamerika); 
 //Südamerika
 function functionSüdamerika() {
     document.querySelector("#titleRegion").innerHTML ="South America";
-    document.querySelector("#h2-1").innerHTML = südamerika2018 + "";
+    document.querySelector(".h2-1").innerHTML = südamerika2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of South America in 2018";
-    document.querySelector("#h2-2").innerHTML = südamerikaVerursacht +"%";
-    document.querySelector("#h2-3").innerHTML = südamerikaVergleichPr +"%";
-    document.querySelector("#h2-4").innerHTML = südamerikaVergleichW + "kg CO2";
+    document.querySelector(".h2-2").innerHTML = südamerikaVerursacht +"%";
+    document.querySelector(".h2-3").innerHTML = südamerikaVergleichPr +"%";
+    document.querySelector(".h2-4").innerHTML = südamerikaVergleichW + "kg CO2";
     
  }
 document.querySelector(".southamerica").addEventListener('click', functionSüdamerika); 
 //Afrika
 function functionAfrika() {
     document.querySelector("#titleRegion").innerHTML ="Africa";
-    document.querySelector("#h2-1").innerHTML = afrika2018 + "";
+    document.querySelector(".h2-1").innerHTML = afrika2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of Afrika in 2018";
-    document.querySelector("#h2-2").innerHTML = afrikaVerursacht +"%";
-    document.querySelector("#h2-3").innerHTML = afrikaVergleichPr +"%";
-    document.querySelector("#h2-4").innerHTML = afrikaVergleichW + "kg CO2";
+    document.querySelector(".h2-2").innerHTML = afrikaVerursacht +"%";
+    document.querySelector(".h2-3").innerHTML = afrikaVergleichPr +"%";
+    document.querySelector(".h2-4").innerHTML = afrikaVergleichW + "kg CO2";
  }
 document.querySelector(".africa").addEventListener('click', functionAfrika); 
 //Asien
 function functionAsien() {
     document.querySelector("#titleRegion").innerHTML ="Asia";
-    document.querySelector("#h2-1").innerHTML = asien2018 + "";
+    document.querySelector(".h2-1").innerHTML = asien2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of Asia in 2018";
-    document.querySelector("#h2-2").innerHTML = asienVerursacht +"%";
-    document.querySelector("#h2-3").innerHTML = asienVergleichPr +"%";
-    document.querySelector("#h2-4").innerHTML = asienVergleichW + "kg CO2";
+    document.querySelector(".h2-2").innerHTML = asienVerursacht +"%";
+    document.querySelector(".h2-3").innerHTML = asienVergleichPr +"%";
+    document.querySelector(".h2-4").innerHTML = asienVergleichW + "kg CO2";
     
  }
 document.querySelector(".asia").addEventListener('click', functionAsien); 
 //Australien
 function functionAustralien() {
     document.querySelector("#titleRegion").innerHTML ="Australia";
-    document.querySelector("#h2-1").innerHTML = australien2018 + "";
+    document.querySelector(".h2-1").innerHTML = australien2018 + "";
     document.querySelector("p").innerHTML ="Emission absolute of Australia in 2018";
-    document.querySelector("#h2-2").innerHTML = australienVerursacht +"%";
-    document.querySelector("#h2-3").innerHTML = australienVergleichPr +"%";
-    document.querySelector("#h2-4").innerHTML = australienVergleichW + "kg CO2";
+    document.querySelector(".h2-2").innerHTML = australienVerursacht +"%";
+    document.querySelector(".h2-3").innerHTML = australienVergleichPr +"%";
+    document.querySelector(".h2-4").innerHTML = australienVergleichW + "kg CO2";
     
  }
 document.querySelector(".australia").addEventListener('click', functionAustralien); 
