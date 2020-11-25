3c09687d4475f973550c62a8b3d61a77f0d3cf9d
christinadae <christina.daeschner@hs-furtwangen.de>
2020-11-25 13:13:12 +0100

    fix NaN

--- Aufgabe6/aufgabe6.js
+++ Aufgabe6/aufgabe6.js
@@ -1,35 +1,59 @@
-var GesamtEmission2018 = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+//Datengrundlage Kontinente
 var europa2008 = 4965.7;
 var europa2018 = 4209.3;
-var europaVerursacht = Math.round((europa2018 / GesamtEmission2018) * 100);
-var europaVergleichPr = Math.round(((europa2018 / europa2008) - 1) * 100);
-var europaVergleichW = Math.round(europa2018 - europa2008);
 var nordamerika2008 = 6600.4;
 var nordamerika2018 = 6035.6;
-var nordamerikaVerursacht = Math.round((nordamerika2018 / GesamtEmission2018) * 100);
-var nordamerikaVergleichPr = Math.round(((nordamerika2018 / nordamerika2008) - 1) * 100);
-var nordamerikaVergleichW = Math.round(nordamerika2018 - nordamerika2008);
 var südamerika2008 = 1132.6;
 var südamerika2018 = 1261.5;
-var südamerikaVerursacht = Math.round((südamerika2018 / GesamtEmission2018) * 100);
-var südamerikaVergleichPr = Math.round(((südamerika2018 / südamerika2008) - 1) * 100);
-var südamerikaVergleichW = Math.round(südamerika2018 - südamerika2008);
 var afrika2008 = 1028;
 var afrika2018 = 1235.5;
-var afrikaVerursacht = Math.round((afrika2018 / GesamtEmission2018) * 100);
-var afrikaVergleichPr = Math.round(((afrika2018 / afrika2008) - 1) * 100);
-var afrikaVergleichW = Math.round(afrika2018 - afrika2008);
 var asien2008 = 12954.7;
 var asien2018 = 16274.1;
-var asienVerursacht = Math.round((asien2018 / GesamtEmission2018) * 100);
-var asienVergleichPr = Math.round(((asien2018 / asien2008) - 1) * 100);
-var asienVergleichW = Math.round(asien2018 - asien2008);
 var australien2008 = 1993;
 var australien2018 = 2100.5;
-var australienVerursacht = Math.round((australien2018 / GesamtEmission2018) * 100);
-var australienVergleichPr = Math.round(((australien2018 / australien2008) - 1) * 100);
-var australienVergleichW = Math.round(australien2018 - australien2008);
 var GesamtEmission2018 = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+//Europa Berechnungen
+// Relative to total world's emission
+var europaVerursacht = Math.round((europa2018 / GesamtEmission2018) * 100);
+// Growth rate between 2008 and 2018 (in %)
+var europaVergleichPr = Math.round(((europa2018 / europa2008) - 1) * 100);
+//Growth rate between 2008 and 2018 (absolute)
+var europaVergleichW = Math.round(europa2018 - europa2008);
+//Nordamerika Berechnungen
+//Relative to total world's emission
+var nordamerikaVerursacht = Math.round((nordamerika2018 / GesamtEmission2018) * 100);
+// Growth rate between 2008 and 2018 (in %)
+var nordamerikaVergleichPr = Math.round(((nordamerika2018 / nordamerika2008) - 1) * 100);
+//Growth rate between 2008 and 2018 (absolute)
+var nordamerikaVergleichW = Math.round(nordamerika2018 - nordamerika2008);
+//Südamerika Berechnungen
+//Relative to total world's emission
+var südamerikaVerursacht = Math.round((südamerika2018 / GesamtEmission2018) * 100);
+// Growth rate between 2008 and 2018 (in %)
+var südamerikaVergleichPr = Math.round(((südamerika2018 / südamerika2008) - 1) * 100);
+//Growth rate between 2008 and 2018 (absolute)
+var südamerikaVergleichW = Math.round(südamerika2018 - südamerika2008);
+// Afrika Berechnungen
+//Relative to total world's emission
+var afrikaVerursacht = Math.round((afrika2018 / GesamtEmission2018) * 100);
+// Growth rate between 2008 and 2018 (in %)
+var afrikaVergleichPr = Math.round(((afrika2018 / afrika2008) - 1) * 100);
+//Growth rate between 2008 and 2018 (absolute)
+var afrikaVergleichW = Math.round(afrika2018 - afrika2008);
+//Asien Berechnungen
+//Relative to total world's emission
+var asienVerursacht = Math.round((asien2018 / GesamtEmission2018) * 100);
+// Growth rate between 2008 and 2018 (in %)
+var asienVergleichPr = Math.round(((asien2018 / asien2008) - 1) * 100);
+//Growth rate between 2008 and 2018 (absolute)
+var asienVergleichW = Math.round(asien2018 - asien2008);
+//Australien Berechnungen
+//Relative to total world's emission
+var australienVerursacht = Math.round((australien2018 / GesamtEmission2018) * 100);
+// Growth rate between 2008 and 2018 (in %)
+var australienVergleichPr = Math.round(((australien2018 / australien2008) - 1) * 100);
+//Growth rate between 2008 and 2018 (absolute)
+var australienVergleichW = Math.round(australien2018 - australien2008);
 //Europa
 function functionEuropa() {
     document.querySelector("#titleRegion").innerHTML = "Europe";
--- Aufgabe6/aufgabe6.ts
+++ Aufgabe6/aufgabe6.ts
@@ -1,7 +1,28 @@
-
+//Datengrundlage Kontinente
 
 var europa2008: number = 4965.7
 var europa2018: number = 4209.3
+
+var nordamerika2008: number = 6600.4;
+var nordamerika2018: number = 6035.6;
+
+var südamerika2008: number = 1132.6;
+var südamerika2018: number = 1261.5;
+
+var afrika2008: number = 1028;
+var afrika2018: number = 1235.5;
+
+var asien2008: number = 12954.7;
+var asien2018: number = 16274.1;
+
+var australien2008: number = 1993;
+var australien2018: number = 2100.5;
+
+var GesamtEmission2018: number = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+
+
+//Europa Berechnungen
+
 // Relative to total world's emission
 var europaVerursacht: number = Math.round((europa2018/GesamtEmission2018)*100)
 // Growth rate between 2008 and 2018 (in %)
@@ -10,40 +31,52 @@ var europaVergleichPr: number = Math.round (((europa2018/europa2008)-1)*100)
 var europaVergleichW: number = Math.round (europa2018-europa2008)
 
 
-var nordamerika2008: number = 6600.4;
-var nordamerika2018: number = 6035.6;
+//Nordamerika Berechnungen
+
+//Relative to total world's emission
 var nordamerikaVerursacht: number = Math.round((nordamerika2018/GesamtEmission2018)*100)
+// Growth rate between 2008 and 2018 (in %)
 var nordamerikaVergleichPr: number = Math.round(((nordamerika2018/nordamerika2008)-1)*100)
+//Growth rate between 2008 and 2018 (absolute)
 var nordamerikaVergleichW: number = Math.round(nordamerika2018-nordamerika2008)
 
+//Südamerika Berechnungen
 
-var südamerika2008: number = 1132.6;
-var südamerika2018: number = 1261.5;
+//Relative to total world's emission
 var südamerikaVerursacht: number = Math.round((südamerika2018/GesamtEmission2018)*100)
+// Growth rate between 2008 and 2018 (in %)
 var südamerikaVergleichPr: number = Math.round(((südamerika2018/südamerika2008)-1)*100)
+//Growth rate between 2008 and 2018 (absolute)
 var südamerikaVergleichW: number = Math.round(südamerika2018-südamerika2008)
 
+// Afrika Berechnungen
 
-var afrika2008: number = 1028;
-var afrika2018: number = 1235.5;
+//Relative to total world's emission
 var afrikaVerursacht: number = Math.round((afrika2018/GesamtEmission2018)*100)
+// Growth rate between 2008 and 2018 (in %)
 var afrikaVergleichPr: number = Math.round(((afrika2018/afrika2008)-1)*100)
+//Growth rate between 2008 and 2018 (absolute)
 var afrikaVergleichW: number = Math.round(afrika2018-afrika2008)
 
+//Asien Berechnungen
 
-var asien2008: number = 12954.7;
-var asien2018: number = 16274.1;
+//Relative to total world's emission
 var asienVerursacht: number = Math.round((asien2018/GesamtEmission2018)*100)
+// Growth rate between 2008 and 2018 (in %)
 var asienVergleichPr: number = Math.round(((asien2018/asien2008)-1)*100)
+//Growth rate between 2008 and 2018 (absolute)
 var asienVergleichW: number = Math.round(asien2018-asien2008)
 
-var australien2008: number = 1993;
-var australien2018: number = 2100.5;
+//Australien Berechnungen
+
+//Relative to total world's emission
 var australienVerursacht: number = Math.round((australien2018/GesamtEmission2018)*100)
+// Growth rate between 2008 and 2018 (in %)
 var australienVergleichPr: number = Math.round(((australien2018/australien2008)-1)*100)
+//Growth rate between 2008 and 2018 (absolute)
 var australienVergleichW: number = Math.round(australien2018-australien2008)
 
-var GesamtEmission2018: number = europa2018 + nordamerika2018 + südamerika2018 + afrika2018 + asien2018 + australien2018;
+
 
 //Europa
 function functionEuropa() {
@@ -55,6 +88,7 @@ function functionEuropa() {
     document.querySelector(".h2-4").innerHTML = europaVergleichW + "kg CO2";
     }
     document.querySelector(".europe").addEventListener('click', functionEuropa);
+
 //Nordamerika
     function functionNordamerika() {
     document.querySelector("#titleRegion").innerHTML ="North America";
@@ -66,6 +100,7 @@ function functionEuropa() {
 
 }
 document.querySelector(".northamerica").addEventListener('click', functionNordamerika); 
+
 //Südamerika
 function functionSüdamerika() {
     document.querySelector("#titleRegion").innerHTML ="South America";
@@ -77,6 +112,7 @@ function functionSüdamerika() {
     
  }
 document.querySelector(".southamerica").addEventListener('click', functionSüdamerika); 
+
 //Afrika
 function functionAfrika() {
     document.querySelector("#titleRegion").innerHTML ="Africa";
@@ -86,7 +122,8 @@ function functionAfrika() {
     document.querySelector(".h2-3").innerHTML = afrikaVergleichPr +"%";
     document.querySelector(".h2-4").innerHTML = afrikaVergleichW + "kg CO2";
  }
-document.querySelector(".africa").addEventListener('click', functionAfrika); 
+document.querySelector(".africa").addEventListener('click', functionAfrika);
+
 //Asien
 function functionAsien() {
     document.querySelector("#titleRegion").innerHTML ="Asia";
@@ -98,6 +135,7 @@ function functionAsien() {
     
  }
 document.querySelector(".asia").addEventListener('click', functionAsien); 
+
 //Australien
 function functionAustralien() {
     document.querySelector("#titleRegion").innerHTML ="Australia";
