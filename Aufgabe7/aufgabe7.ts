7d29c0c76a581978b16471188a057f84cbce3146
christinadae <christina.daeschner@hs-furtwangen.de>
2020-12-02 15:11:08 +0100

    edit

--- Aufgabe7/aufgabe7.ts
+++ Aufgabe7/aufgabe7.ts
@@ -1,71 +1,53 @@
-var sound1:HTMLAudioElement = new Audio('A.mp3');
-sound1.play();
+var sound = [];
+sound [1] = new Audio('./assets/A.mp3'); 
+sound [2] = new Audio('./assets/C.mp3'); 
+sound [3] = new Audio('./assets/F.mp3'); 
+sound [4] = new Audio('./assets/G.mp3'); 
+sound [5] = new Audio('./assets/hihat.mp3'); 
+sound [6] = new Audio('./assets/kick.mp3'); 
+sound [7] = new Audio('./assets/laugh-1.mp3'); 
+sound [8] = new Audio('./assets/laugh-2.mp3'); 
+sound [9] = new Audio('./assets/snare.mp3'); 
 
-var sound2:HTMLAudioElement = new Audio('C.mp3');
-sound2.play();
+// Event-Listener für Sounds
 
-var sound3:HTMLAudioElement = new Audio('F.mp3');
-sound3.play();
-
-var sound4:HTMLAudioElement = new Audio('G.mp3');
-sound4.play();
-
-var sound5:HTMLAudioElement = new Audio('hihat.mp3');
-sound5.play();
-
-var sound6:HTMLAudioElement = new Audio('kick.mp3');
-sound6.play();
-
-var sound7:HTMLAudioElement = new Audio('laugh-1.mp3');
-sound7.play();
-
-var sound8:HTMLAudioElement = new Audio('laugh-2.mp3');
-sound8.play();
-
-var sound9:HTMLAudioElement = new Audio('snare.mp3');
-sound9.play();
-
-function playSample () {
-  sound.play(); 
-
-  document.querySelector(".button1").addEventListener("click", function () {
-    playSample(sound);
-});
-
-
-}
 document.querySelector(".button1").addEventListener("click", function () {
-    playSample(1)
+    playSample(1);
 });
 
 document.querySelector(".button2").addEventListener("click", function () {
-    playSample(sound2)
+    playSample(2);
 });
 
 document.querySelector(".button3").addEventListener("click", function () {
-    playSample(sound3)
+    playSample(3);
 });
 
 document.querySelector(".button4").addEventListener("click", function () {
-    playSample(sound4)
+    playSample(4);
 });
 
 document.querySelector(".button5").addEventListener("click", function () {
-    playSample(sound5)
+    playSample(5);
 });
 
 document.querySelector(".button6").addEventListener("click", function () {
-    playSample(sound6)
+    playSample(6);
 });
 
 document.querySelector(".button7").addEventListener("click", function () {
-    playSample(sound7)
+    playSample(7);
 });
 
 document.querySelector(".button8").addEventListener("click", function () {
-    playSample(sound8)
+    playSample(8);
 });
 
 document.querySelector(".button9").addEventListener("click", function () {
-    playSample(sound9)
+    playSample(9);
 });
+
+// PlaySample Function
+  function playSample(b) {
+    sound[b].play();
+  };
