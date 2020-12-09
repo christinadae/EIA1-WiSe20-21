422c0f73a078dfd70bf6ecdf7d088d226145eb41
christinadae <christina.daeschner@hs-furtwangen.de>
2020-12-09 15:09:56 +0100

    edit

--- Aufgabe8/aufgabe8.js
+++ Aufgabe8/aufgabe8.js
@@ -1,5 +1,6 @@
 // Array Buttons
 // tslint:disable-next-line: typedef
+// tslint:disable-next-line: typedef
 var button = [];
 button[1] = new Audio("./assets/A.mp3");
 button[2] = new Audio("./assets/C.mp3");
@@ -13,56 +14,56 @@ button[9] = new Audio("./assets/snare.mp3");
 // Event-Listener für Sounds
 // tslint:disable-next-line: typedef
 document.querySelector(".button1").addEventListener("click", function () {
-    playSample(1);
+    playSample(button[1]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button2").addEventListener("click", function () {
-    playSample(2);
+    playSample(button[2]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button3").addEventListener("click", function () {
-    playSample(3);
+    playSample(button[3]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button4").addEventListener("click", function () {
-    playSample(4);
+    playSample(button[4]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button5").addEventListener("click", function () {
-    playSample(5);
+    playSample(button[5]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button6").addEventListener("click", function () {
-    playSample(6);
+    playSample(button[6]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button7").addEventListener("click", function () {
-    playSample(7);
+    playSample(button[7]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button8").addEventListener("click", function () {
-    playSample(8);
+    playSample(button[8]);
 });
 // tslint:disable-next-line: typedef
 document.querySelector(".button9").addEventListener("click", function () {
-    playSample(9);
+    playSample(button[9]);
 });
 // PlaySample Funktion
 // tslint:disable-next-line: typedef
-function playSample(a) {
-    button[a].play();
+function playSample(x) {
+    x.play();
 }
 // Array Beat
 // tslint:disable-next-line: variable-name
 // tslint:disable-next-line: typedef
-var sample = [];
-sample[0] = new Audio("./assets/kick.mp3");
-sample[1] = new Audio("./assets/snare.mp3");
-sample[2] = new Audio("./assets/hihat.mp3");
-sample[3] = new Audio("./assets/laugh-2.mp3");
+// tslint:disable-next-line: typedef
+var sample = [5, 6, 9];
+// tslint:disable-next-line: typedef
 var index = 0;
 var intervall;
-var record = false;
+var record;
+// tslint:disable-next-line: typedef
+var x;
 // Variablen Definition für die Buttons
 var play = document.getElementById("playButton");
 var stoppen = document.getElementById("stopButton");
@@ -74,20 +75,14 @@ stoppen.addEventListener("click", function () {
     clearInterval(intervall);
 });
 // Event-Listener für PlayButton
-play.addEventListener("click", function () {
-    // tslint:disable-next-line: typedef
-    intervall = setInterval(function () {
-        for (var index = 0; index <= sample.length; index++) {
-            sample[index].play();
-        }
-    }, 600);
-});
 // Klick auf PlayButton wird zu StopButton und andersrum
 play.addEventListener("click", function () {
+    playandstop(true);
     this.classList.add("is-hidden");
     stoppen.classList.remove("is-hidden");
 });
 stoppen.addEventListener("click", function () {
+    playandstop(false);
     this.classList.add("is-hidden");
     play.classList.remove("is-hidden");
 });
@@ -95,13 +90,46 @@ function playstop(first, second) {
     first.classList.add(".is-hidden");
     second.classList.remove(".is-hiden");
 }
+// Funktion fürs Löschen
+löschen.addEventListener("click", function () {
+    deleteBeat();
+});
+// tslint:disable-next-line: typedef
+function deleteBeat() {
+    sample.splice(0, sample.length);
+}
+// Funktion fürs Aufnehmen
+// tslint:disable-next-line: typedef
+function recordBeat() {
+    sample.push();
+}
 // Klick auf RecordButton
 aufnehmen.addEventListener("click", function () {
     this.classList.add("is-hidden");
     aufnehmenstopp.classList.remove("is-hidden");
+    recordBeat();
 });
 aufnehmenstopp.addEventListener("click", function () {
     this.classList.add("is-hidden");
     aufnehmen.classList.remove("is-hidden");
 });
+// Funktion PlayStop
+// tslint:disable-next-line: typedef
+function playandstop(a) {
+    if (a == true) {
+        // tslint:disable-next-line: typedef
+        intervall = setInterval(function () {
+            if (x < sample.length) {
+                playSample(sample[x]);
+                x++;
+            }
+            else {
+                x = 0;
+            }
+        }, 600);
+    }
+    else {
+        clearInterval(intervall);
+    }
+}
 //# sourceMappingURL=aufgabe8.js.map
--- Aufgabe8/aufgabe8.ts
+++ Aufgabe8/aufgabe8.ts
@@ -1,5 +1,7 @@
 // Array Buttons
 // tslint:disable-next-line: typedef
+
+// tslint:disable-next-line: typedef
 var button = [];
 button [1] = new Audio("./assets/A.mp3"); 
 button [2] = new Audio("./assets/C.mp3"); 
@@ -14,78 +16,79 @@ button [9] = new Audio("./assets/snare.mp3");
 // Event-Listener für Sounds
 // tslint:disable-next-line: typedef
 document.querySelector(".button1").addEventListener("click", function () {
-    playSample(1);
+    playSample(button[1]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button2").addEventListener("click", function () {
-    playSample(2);
+    playSample(button[2]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button3").addEventListener("click", function () {
-    playSample(3);
+    playSample(button[3]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button4").addEventListener("click", function () {
-    playSample(4);
+    playSample(button[4]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button5").addEventListener("click", function () {
-    playSample(5);
+    playSample(button[5]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button6").addEventListener("click", function () {
-    playSample(6);
+    playSample(button[6]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button7").addEventListener("click", function () {
-    playSample(7);
+    playSample(button[7]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button8").addEventListener("click", function () {
-    playSample(8);
+    playSample(button[8]);
 });
 
 // tslint:disable-next-line: typedef
 document.querySelector(".button9").addEventListener("click", function () {
-    playSample(9);
+    playSample(button[9]);
 });
 
 // PlaySample Funktion
 // tslint:disable-next-line: typedef
-function playSample (a: string | number) {
-    button[a].play();
+function playSample (x) {
+    x.play();
     }
 
 // Array Beat
 
 // tslint:disable-next-line: variable-name
 // tslint:disable-next-line: typedef
-var sample = [];
-
-sample [0] = new Audio("./assets/kick.mp3");
-sample [1] = new Audio("./assets/snare.mp3");
-sample [2] = new Audio("./assets/hihat.mp3"); 
-sample [3] = new Audio("./assets/laugh-2.mp3"); 
 
 
-var index: number = 0;
+// tslint:disable-next-line: typedef
+var sample = [5, 6, 9];
+
+// tslint:disable-next-line: typedef
+var index = 0;
 var intervall: number; 
-var record: boolean = false;
+var record: boolean; 
+// tslint:disable-next-line: typedef
+var x; 
+
 
 // Variablen Definition für die Buttons
 
-const play: HTMLElement = document.getElementById("playButton");
-const stoppen: HTMLElement = document.getElementById("stopButton");
-const aufnehmen: HTMLElement = document.getElementById("recordButton");
-const aufnehmenstopp: HTMLElement = document.getElementById("whileRecord");
-const löschen: HTMLElement = document.getElementById("deleteButton");
+var play: HTMLElement = document.getElementById("playButton");
+var stoppen: HTMLElement = document.getElementById("stopButton");
+var aufnehmen: HTMLElement = document.getElementById("recordButton");
+var aufnehmenstopp: HTMLElement = document.getElementById("whileRecord");
+var löschen: HTMLElement = document.getElementById("deleteButton");
 
 // Bei Klick auf StopButton wird der aktuelle Beat pausiert
 
@@ -93,26 +96,19 @@ stoppen.addEventListener("click", function(): void {
 clearInterval(intervall);
 
 });
+
 // Event-Listener für PlayButton
-play.addEventListener("click", function (): void {
-    // tslint:disable-next-line: typedef
-    intervall = setInterval(function () {
-        for (var index: number = 0; index <= sample.length; index++) {
-           sample[index].play();
-        }
-    },                      600);
-
-});
-
 
 // Klick auf PlayButton wird zu StopButton und andersrum
 
 play.addEventListener("click", function (): void {
+playandstop(true);
 this.classList.add("is-hidden");
 stoppen.classList.remove("is-hidden");
 });
 
 stoppen.addEventListener("click", function (): void {
+    playandstop(false);
     this.classList.add("is-hidden");
     play.classList.remove("is-hidden");
 });
@@ -121,12 +117,29 @@ function playstop(first: HTMLElement, second: HTMLElement): void {
 first.classList.add(".is-hidden");
 second.classList.remove(".is-hiden");
 }
+// Funktion fürs Löschen
+
+löschen.addEventListener("click", function (): void {
+    deleteBeat();
+    });
+// tslint:disable-next-line: typedef
+function deleteBeat () {
+sample.splice(0, sample.length);
+}
+
+// Funktion fürs Aufnehmen
+// tslint:disable-next-line: typedef
+function recordBeat () {
+sample.push();
+}
+
 
 // Klick auf RecordButton
 
 aufnehmen.addEventListener ("click", function (): void {
 this.classList.add("is-hidden");
 aufnehmenstopp.classList.remove("is-hidden");
+recordBeat();
 });
 
 aufnehmenstopp.addEventListener("click", function (): void {
@@ -135,6 +148,24 @@ aufnehmenstopp.addEventListener("click", function (): void {
 });
 
 
+// Funktion PlayStop
+// tslint:disable-next-line: typedef
+function playandstop(a) {
+    if (a == true) {
+        // tslint:disable-next-line: typedef
+        intervall = setInterval(function () {
+            if (x < sample.length) {
+                playSample(sample[x]);
+                x++;
+            }
+            else {
+                x = 0;
+            }
+        },                      600);
+    }
+    else {
+        clearInterval(intervall);
+    }}
 
 
 
