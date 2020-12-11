1ab7adc7fe8d7368dd70a4da285bd297a2509864
christinadae <christina.daeschner@hs-furtwangen.de>
2020-12-11 16:11:42 +0100

    Update aufgabe8.ts

--- Aufgabe8/aufgabe8.ts
+++ Aufgabe8/aufgabe8.ts
@@ -1,6 +1,5 @@
 // Array Buttons
 
-
 // tslint:disable-next-line: typedef
 var button = [];
 button [1] = new Audio("./assets/A.mp3"); 
@@ -14,57 +13,47 @@ button [8] = new Audio("./assets/laugh-2.mp3");
 button [9] = new Audio("./assets/snare.mp3"); 
 
 // Event-Listener für Sounds
-// tslint:disable-next-line: typedef
-document.querySelector(".button1").addEventListener("click", function () {
+
+document.querySelector(".button1").addEventListener("click", function (): void {
     playSample(button[1]);
     recordBeat(button[1]);
     
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button2").addEventListener("click", function () {
+document.querySelector(".button2").addEventListener("click", function (): void {
     playSample(button[2]);
     recordBeat(button[2]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button3").addEventListener("click", function () {
+document.querySelector(".button3").addEventListener("click", function (): void {
     playSample(button[3]);
     recordBeat(button[3]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button4").addEventListener("click", function () {
+document.querySelector(".button4").addEventListener("click", function (): void {
     playSample(button[4]);
     recordBeat(button[4]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button5").addEventListener("click", function () {
+document.querySelector(".button5").addEventListener("click", function (): void {
     playSample(button[5]);
     recordBeat(button[5]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button6").addEventListener("click", function () {
+document.querySelector(".button6").addEventListener("click", function (): void  {
     playSample(button[6]);
     recordBeat(button[6]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button7").addEventListener("click", function () {
+document.querySelector(".button7").addEventListener("click", function (): void {
     playSample(button[7]);
     recordBeat(button[7]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button8").addEventListener("click", function () {
+document.querySelector(".button8").addEventListener("click", function (): void {
     playSample(button[8]);
     recordBeat(button[8]);
 });
-// tslint:disable-next-line: typedef
-document.querySelector(".button9").addEventListener("click", function () {
+document.querySelector(".button9").addEventListener("click", function (): void {
     playSample(button[9]);
     recordBeat(button[9]);
 });
 
 // PlaySample Funktion
-
-// tslint:disable-next-line: typedef
-function playSample (x: HTMLAudioElement) {
+function playSample (x: HTMLAudioElement): void  {
 x.play();
 }
 
@@ -130,9 +119,7 @@ stoppen.addEventListener("click", function (): void {
     
 
 // Funktion fürs Aufnehmen
-
-// tslint:disable-next-line: typedef
-function recordBeat (x: HTMLAudioElement) {
+function recordBeat (x: HTMLAudioElement): void {
     console.log(recordactive);
     if (recordactive == true) {
     sample.push(x);
@@ -150,16 +137,15 @@ console.log(sample.length);
 // tslint:disable-next-line: typedef
 function PlayBeat(a): void {
     if (a == true) {
-        // tslint:disable-next-line: typedef
-        intervall = setInterval(function (): void {
-            if (x < sample.length) {
-                playSample(sample[x]);
-                x++;
-            }
-            else {
-                x = 0;
-            }
-        },                      500);
+    intervall = setInterval(function (): void {
+    if (x < sample.length) {
+    playSample(sample[x]);
+    x++;
+    }
+    else {
+    x = 0;
+    }
+    },                      500);
     }
     else {
         clearInterval(intervall);
