c4add6fecb04475f7ef782ff51d90a3d9b6b23e6
christinadae <christina.daeschner@hs-furtwangen.de>
2020-12-14 17:45:34 +0100

    random

--- Aufgabe9/aufgabe9.js
+++ Aufgabe9/aufgabe9.js
@@ -1,2 +1,12 @@
 var items = [];
+var listactive;
+document.getElementById('#ToDoList').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+    }
+});
+function ToDoList() {
+    if (listactive == true) {
+        items.push();
+    }
+}
 //# sourceMappingURL=aufgabe9.js.map
--- Aufgabe9/aufgabe9.ts
+++ Aufgabe9/aufgabe9.ts
@@ -1,1 +1,16 @@
 var items: string [] = [];
+
+var listactive: boolean;
+
+
+document.getElementById('#ToDoList').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+    }
+});
+
+function ToDoList () {
+    if (listactive == true) {
+        items.push();
+       
+}}
+
