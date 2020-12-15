b6c0633639f66fe4ca1095ff6a04be86b5d5f508
christinadae <christina.daeschner@hs-furtwangen.de>
2020-12-15 14:56:01 +0100

    test

--- Aufgabe9/probe9.js
+++ Aufgabe9/probe9.js
@@ -1,11 +1,12 @@
-var myList = document.getElementsByTagName("LI");
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myList.length; i++) {
+for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    myList[i].appendChild(span);
+    myNodelist[i].appendChild(span);
 }
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
@@ -17,10 +18,10 @@ for (i = 0; i < close.length; i++) {
     };
 }
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector("ul");
-list.addEventListener("click", function (ev) {
-    if (ev.target.tagName === ("LI")) {
-        ev.target.classList.toggle("checked");
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
     }
 }, false);
 // Create a new list item when clicking on the "Add" button
@@ -29,7 +30,7 @@ function newElement() {
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
-    if (inputValue === "") {
+    if (inputValue === '') {
         alert("You must write something!");
     }
     else {
@@ -48,4 +49,4 @@ function newElement() {
         };
     }
 }
-//# sourceMappingURL=aufgabe9.js.map
+//# sourceMappingURL=probe9.js.map
--- Aufgabe9/probe9.ts
+++ Aufgabe9/probe9.ts
@@ -1,28 +1,29 @@
-var myList: HTMLCollectionOf<Element> = document.getElementsByTagName("LI");
-var i: number;
-for (i = 0; i < myList.length; i++) {
-  var span: HTMLElement = document.createElement("SPAN");
-  var txt: Text = document.createTextNode("\u00D7");
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  myList[i].appendChild(span);
+  myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
-var close: HTMLCollectionOf<Element>  = document.getElementsByClassName("close");
-var i: number;
+var close = document.getElementsByClassName("close");
+var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function(): void {
-    var div: HTMLElement = this.parentElement;
+  close[i].onclick = function() {
+    var div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list: HTMLUListElement = document.querySelector("ul");
-list.addEventListener("click", function(ev: MouseEvent): void {
-  if (ev.target.tagName === ("LI") {
-    ev.target.classList.toggle("checked");
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
   }
 }, false);
 
@@ -32,7 +33,7 @@ function newElement() {
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === "") {
+  if (inputValue === '') {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
@@ -46,7 +47,7 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function(): void {
+    close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
